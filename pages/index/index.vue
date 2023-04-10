00ff9272823e5871907058cab556427d63498462
Lucia <12599044+luciaLiang@user.noreply.gitee.com>
2023-04-11 02:00:53 +0800

    完成主要icon部分的样式

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -2,16 +2,19 @@
 	<view class="container">
 		<nav-bar></nav-bar>
 		<swiper-wrap></swiper-wrap>
+		<multi-card></multi-card>
 	</view>
 </template>
 
 <script>
 	import swiperWrap from '@/pages/swiper/swiper.vue'
 	import navBar from '@/pages/nav-bar/nav-bar.vue'
+	import multiCard from '@/pages/multicard/multicard.vue'
 	export default {
 		components: {
 			swiperWrap,
-			navBar
+			navBar,
+			multiCard
 		},
 		data() {
 			return {
---
+++ pages/multicard/multicard.vue
@@ -0,0 +1,71 @@
+<template>
+	<view class="multi-card">
+		<uni-card>
+			<view class="row1">
+				<view class="card" v-for="(item, index) in info[0]" :key="index">
+					<img :src="item.src" alt="">
+				</view>
+			</view>
+			<view class="row2">
+				<view class="card" v-for="(item, index) in info[1]" :key="index">
+					<img :src="item.src" alt="">
+				</view>
+			</view>
+			
+		</uni-card>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				info:{0:[
+						{src: '/static/multi-icon/1.png', text: '米金商城'},
+						{src: '/static/multi-icon/2.png', text: '小米众筹'},
+						{src: '/static/multi-icon/3.png', text: '手机'},
+						{src: '/static/multi-icon/4.png', text: '以旧换新'},
+						{src: '/static/multi-icon/5.png', text: '小米上新'}
+					],
+					1:[
+						{src: '/static/multi-icon/6.png', text: '智能'},
+						{src: '/static/multi-icon/7.png', text: '笔记本热卖'},
+						{src: '/static/multi-icon/8.png', text: '电视热卖'},
+						{src: '/static/multi-icon/9.png', text: '洗衣机热卖'},
+						{src: '/static/multi-icon/10.png', text: '米粉卡'}
+					]
+				}
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style lang="scss">
+	.multi-card{
+		padding: 0 !important;margin: 0 !important;
+		.uni-card{
+			padding: 0 !important;margin: 0 !important;
+			border: none;
+			box-shadow: none !important;
+			.uni-card__content{
+				padding: 0 !important;margin: 0 !important;
+				display: flex;
+				flex-direction: row;
+				flex-wrap: wrap;
+				.row1,.row2{
+					display: flex;
+				}
+				.card{
+					img{
+						width: 100%;
+						
+					}
+				}
+			}
+		}
+		 
+	}
+</style>
