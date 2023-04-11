815ff30bde5c6737a80717ab82fb13832493bdc3
Lucia <12599044+luciaLiang@user.noreply.gitee.com>
2023-04-11 15:22:53 +0800

    完成大图部分的样式

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -3,6 +3,7 @@
 		<nav-bar></nav-bar>
 		<swiper-wrap></swiper-wrap>
 		<multi-card></multi-card>
+		<rec></rec>
 	</view>
 </template>
 
@@ -10,11 +11,13 @@
 	import swiperWrap from '@/pages/swiper/swiper.vue'
 	import navBar from '@/pages/nav-bar/nav-bar.vue'
 	import multiCard from '@/pages/multicard/multicard.vue'
+	import rec from '@/pages/rec/rec.vue'
 	export default {
 		components: {
 			swiperWrap,
 			navBar,
-			multiCard
+			multiCard,
+			rec
 		},
 		data() {
 			return {
---
+++ pages/rec/rec.vue
@@ -0,0 +1,65 @@
+<template>
+	<view class="grid-container rec">
+		<img :class="item.class" v-for="(item, index) in info" :key="index" :src="item.src" alt="item.text">
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				info: [{
+						src: '/static/rec-img/1.png',
+						text: '1',
+						class: 'box1'
+					},
+					{
+						src: '/static/rec-img/2.png',
+						text: '2',
+						class: 'box2'
+					},
+					{
+						src: '/static/rec-img/3.png',
+						text: '3',
+						class: 'box3'
+					},
+				]
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss">
+	.rec {
+		margin: 10px 0;
+
+		&.grid-container {
+			display: grid;
+
+			.box1 {
+				grid-area: box1;
+			}
+
+			.box2 {
+				grid-area: box2;
+			}
+
+			.box3 {
+				grid-area: box3;
+			}
+
+			grid-template-columns: 1fr 1fr;
+			grid-template-rows: 1fr 1fr;
+			grid-template-areas: "box1  box2"
+			"box1  box3";
+			grid-column-gap: 3px;
+			grid-row-gap: 2px;
+			img {
+				width: 100%;
+			}
+		}
+	}
+</style>
