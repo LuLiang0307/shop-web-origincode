b1bdd86739b99c1a31ccdeb06d2be41f127b35ee
Lucia <12599044+luciaLiang@user.noreply.gitee.com>
2023-04-16 02:05:35 +0800

    初步完成分类部分的样式

--- pages/category/category.vue
+++ pages/category/category.vue
@@ -1,22 +1,47 @@
 <template>
-	<view>
-		
+	<view class="category">		
+		<uni-card>
+			<uni-nav-bar :fixed="true" background-color="#F5F5F6" status-bar leftWidth="0px" rightWidth="0px" title="自定义导航栏">
+				<view class="input-view">
+					<uni-easyinput prefixIcon="search" v-model="value" placeholder="搜索商品名称" @iconClick="iconClick"
+						:trim="true"></uni-easyinput>
+				</view>
+			</uni-nav-bar>
+			<menu-side-bar></menu-side-bar>
+			</uni-card>
 	</view>
 </template>
 
 <script>
+	import menuSideBar from '../menusidebar/menusidebar.vue'
 	export default {
+		components:{
+			menuSideBar
+		},
 		data() {
 			return {
-				
+				value: '',
 			}
 		},
 		methods: {
-			
+			iconClick() {}
 		}
 	}
 </script>
 
-<style>
-
+<style lang="scss">
+	.category {
+		background-color: #F5F5F6;
+			.input-view {
+				/* #ifndef APP-PLUS-NVUE */
+				display: flex;
+				/* #endif */
+				flex-direction: row;
+				align-items: center;
+				flex: 1;
+				flex-wrap: nowrap;
+				width: 100%;
+			
+		}
+	}
 </style>
